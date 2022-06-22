3c46c6faff1907c8456f45e1d2a89045f19aeeb7
Oleg Steba <oleg.steba@gmail.com>
2022-06-22 20:40:16 +0500

    test

---
+++ first/apps/books/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import HttpResponse
+from django.shortcuts import render
+from .models import Book
+
+
+def get_books_list(requests):
+    book = Book.objects.get(pk=1)
+    return HttpResponse(f"<h1>{book.book_name}</h1>")
---
+++ first/apps/library/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import HttpResponse
+from django.shortcuts import render
+
+def get_library_list(requests):
+    return HttpResponse('Добро пожаловать в раздел библиотек!')
