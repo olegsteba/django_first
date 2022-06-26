e6abead2aa32afe7788c3a848618573db2c7801f
Oleg Steba <oleg.steba@gmail.com>
2022-06-26 12:46:53 +0500

    views book list

--- first/apps/books/views.py
+++ first/apps/books/views.py
@@ -6,8 +6,4 @@ from .models import Book
 def get_books_list(requests):
     book = Book.objects.get(id_publishing_house__publishing_house_name="Дрофа")
     print(book.book_name)
-<<<<<<< HEAD
     return HttpResponse(f"<h1>{book.book_name}</h1>")
-=======
-    return HttpResponse(f"<h1>ОК</h1>")
->>>>>>> f4738361824e0b3a092cb0bd6cfc1d9c97da277c
