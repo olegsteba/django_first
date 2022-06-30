93c322797a962ee125acfabedb33ceccaab93dbe
Oleg Steba <oleg.steba@gmail.com>
2022-06-30 20:56:52 +0500

    book author publish

--- first/apps/books/views.py
+++ first/apps/books/views.py
@@ -3,9 +3,23 @@ from .models import Book, PublishingHouse, Author
 
 
 def get_books_list(requests):
-    books = Book.objects.all()
+    books = Book.objects.all().values(
+        "book_name",
+        "date_creation",
+        "description",
+        "id_publishing_house__publishing_house_name",
+        "id_publishing_house__address",
+        "id_publishing_house__contact_phone",
+        "author__first_name",
+        "author__last_name",
+        "author__country",
+    )
     context = {
         'title': 'Книги',
-        'books': books
+        'books': books,
+        'sale': True,
     }
     return render(requests, 'books/index.html', context)
+    return render(requests, 'books/index.html', {'title': 'Книги', 'books': books})
+
+
